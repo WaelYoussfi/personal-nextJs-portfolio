2ea41022f8c6cd7c5027869023fd1ec568b1b44d
Wael Youssfi <youssfi.wael1997@gmail.com>
2021-07-24 02:57:24 +0100

    projects section implemented

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section raw nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Elegantly Crafted <br />
+        Web Experience
+      </SectionTitle>
+      <SectionText>
+        Welcome to my Portfolio! My name is Wael and I'm a software engineering
+        student. I love web development and I thrive in the ever evolving,
+        complexe yet attractive web and mobile development universe.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location = "https://www.linkedin.com/in/wyoussfi/")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
